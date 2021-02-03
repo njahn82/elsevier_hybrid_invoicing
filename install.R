0cb68084ac2954cde6d7de2f0109738904762c4d
Najko Jahn <najko.jahn@gmail.com>
2021-02-03 17:12:13 +0100

    fix binder setup

--- install.R
+++ install.R
@@ -1,6 +1,4 @@
 # only packages used in manuscript.Rmd
 my_packages <- c("tidyverse", "here", "cowplot", "gt", 
                  "janitor", "kableExtra", "stringr")
-install.packages(my_packages,
-                 repos='https://mran.microsoft.com/snapshot/2021-01-31/', 
-                 dependencies=TRUE)
+install.packages(my_packages, dependencies=TRUE)
