260caf2601f6677706786e91aecab61bbc8684aa
Najko Jahn <najko.jahn@gmail.com>
2021-02-04 10:54:20 +0100

    add latex supprt

--- install.R
+++ install.R
@@ -1,4 +1,5 @@
 # only packages used in manuscript.Rmd
 my_packages <- c("tidyverse", "here", "cowplot", "gt", 
-                 "janitor", "kableExtra", "stringr")
+                 "janitor", "kableExtra", "stringr", "tinytex")
 install.packages(my_packages, dependencies=TRUE)
+tinytex::install_tinytex()
